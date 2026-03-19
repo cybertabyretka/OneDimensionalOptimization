ecb9939d04f2612e9ebc65d4597f4af6987a330d
Vlad Kalashnikov <kalashnikovv537@gmail.com>
2026-03-19 21:36:14 +0500

    ref: add comments

--- config_reader.h
+++ config_reader.h
@@ -3,18 +3,29 @@
 
 #include <iostream>
 
+// Configuration structure for the optimization algorithm
 struct Config {
+    // Number of initial points for the search
     int n_initial_points = 5;
+    // Initial left endpoint for localization
     double init_a = -5.0;
+    // Initial right endpoint for localization
     double init_b = 5.0;
+    // Initial step size for expanding the search during localization
     double initial_step = 0.5;
+    // Factor by which to expand the search interval during localization
     double expand_factor = 2.0;
+    // Maximum expansion factor for the search interval
     double max_expand = 1e6;
+    // Stopping criterion for findmin
     enum StopType { BY_ARGUMENT = 0, BY_FUNCTION = 1, BY_GRADIENT = 2 } stop_type = BY_ARGUMENT;
+    // Tolerance for stopping criterion
     double tol = 1e-6;
+    // Maximum number of iterations for localization and findmin
     int max_iters = 1000;
 };
 
+// Function to load configuration from an XML file
 Config load_config_from_xml(const std::string& filename);
 
 #endif // CONFIG_READER_H
--- golden_opt.h
+++ golden_opt.h
@@ -1,36 +1,39 @@
 #ifndef GOLDEN_OPT_H
 #define GOLDEN_OPT_H
 
-#include <functional>
-#include <string>
-#include <tuple>
-
 #include "config_reader.h"
 
+// Structure to hold the bracketing interval for the minimum
 struct Tripple {
     double a;
     double b;
     double c;
 };
 
+// Class for performing golden section search optimization
 class Fop {
 public:
+    // Pointer to the objective function
     double (*fp)(double x);
+    // Configuration parameters for the optimization
+    Config cfg;
 
+    // Constructor with function pointer only, not initializing config
     Fop(double (*fp)(double x));
+    // Constructor with function pointer and config
     Fop(double (*fp)(double x), const Config& cfg);
 
+    // Method to find a bracketing interval containing a local minimum
     Tripple localize();
-
-    double findmin(Tripple bracket);
-
+    // Method to perform golden section search within the given bracket to find the minimum
+    double findmin(Tripple bracket); 
+    // Method to compute the numerical derivative of the objective function at a given point
     double derivative(double x, double h = 1e-6) const;
-
+    // Method to update or initialize the configuration parameters
     void set_config(const Config& cfg);
 
 private:
-    Config cfg;
-
+    // Helper method to validate the configuration parameters
     bool check_cfg(Config cfg_);
 };
 
